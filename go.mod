7d598aa175e1a9d199680fe56def29523f821d1c
Emil Hein Eriksen <emilhein1@gmail.com>
2019-10-29 12:40:11 +0100

    init commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/heroku/go-getting-started
+
+go 1.12
+
+require (
+	github.com/aws/aws-sdk-go v1.25.21 // indirect
+	github.com/emilhein/go-aws-webserver v1.0.0
+	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/net v0.0.0-20191028085509-fe3aa8a45271 // indirect
+)
---
+++ vendor/github.com/emilhein/go-aws-webserver/webserver/s3.go
@@ -0,0 +1,34 @@
+package webserver
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/emilhein/go-aws-utility/util/services"
+)
+
+type Input struct {
+	Bucket    string   `json:"bucket"`
+	Filepaths []string `json:"filepaths"`
+}
+
+/*
+Takes JSON input "Bucket" & "Filepaths"
+
+
+*/
+func GetS3Files(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	var input Input
+	_ = json.NewDecoder(r.Body).Decode(&input)
+	var lowercasesBucket = strings.ToLower(input.Bucket)
+	fmt.Printf("Getting: %s/%s \n", lowercasesBucket, input.Filepaths)
+
+	inputToFunc := services.FilesInput{Bucket: lowercasesBucket, FileNames: input.Filepaths}
+	returnValues := services.GetS3Files(inputToFunc)
+	json.NewEncoder(w).Encode(returnValues)
+
+}
